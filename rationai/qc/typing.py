3a9de794c5ea95e122197b9e53e2f3ecc55f4650
Tomáš Balák <525126@mail.muni.cz>
2024-11-05 13:53:03 +0100

    feat: stain type alias and descriptions

--- rationai/qc/typing.py
+++ rationai/qc/typing.py
@@ -5,4 +5,16 @@ from numpy.typing import NDArray
 
 
 RGBImage: TypeAlias = NDArray[np.uint8]
+"""
+Three channel RGB image represented by a numpy array.
+"""
+
+Stain: TypeAlias = NDArray[np.float64]
+"""
+Single stain vector (of 3 values) represented by a numpy array.
+"""
+
 QcValues: TypeAlias = dict[str, Any]
+"""
+Dictionary with values computed by a QC function.
+"""
