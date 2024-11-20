72317df6a69c8ce7959606ca4ec1a941909ced13
Tomáš Balák <525126@mail.muni.cz>
2024-11-20 16:37:58 +0100

    feat: compute only one diff if the detected stains are too close

--- rationai/qc/staining/staining_difference.py
+++ rationai/qc/staining/staining_difference.py
@@ -1,4 +1,8 @@
-from rationai.qc.staining.color_difference import closest_difference, reference_stain
+from rationai.qc.staining.color_difference import (
+    closest_difference,
+    color_difference,
+    reference_stain,
+)
 from rationai.qc.typing import QcValues, Stain
 from rationai.staining import ColorConversion
 
@@ -8,7 +12,8 @@ def staining_difference(
     stain1: Stain,
     stain2: Stain,
     local_threshold: float,
-    color_difference: str,
+    single_stain_threshold: float,
+    color_difference_method: str,
 ) -> QcValues:
     """Decides if the given stains are close engough to the reference stains.
 
@@ -19,7 +24,11 @@ def staining_difference(
         stain2: Second dominant stain.
         local_threshold: Threshold that determines if the specified stains
             are close enough to the expected stains.
-        color_difference: Method used for computing the color difference.
+        single_stain_threshold: Threshold that determines if `stain1` and `stain2`
+            are too close to eachother. If so, the region in which the stains were
+            detected is assumed to be stained only by a single stain and the second
+            color difference is set to 0.
+        color_difference_method: Method used for computing the color difference.
             Options: `ciede_2000`, `ciede_94`, `cie_76`.
 
     Returns:
@@ -45,7 +54,7 @@ def staining_difference(
     stain1, stain2 = result["stain1"], result["stain2"]
 
     result = staining_difference(
-        ColorConversion.RGB2HDR, stain1, stain2, 33, "ciede_2000"
+        ColorConversion.RGB2HDR, stain1, stain2, 33, 20, "ciede_2000"
     )
     print(result["correct_staining"])
     print(result["stain_diff1"], result["stain_diff2"])
@@ -56,7 +65,16 @@ def staining_difference(
     ref1 = reference_stain(conversion=conversion, index=0)
     ref2 = reference_stain(conversion=conversion, index=1)
 
-    diff1, diff2 = closest_difference(stain1, stain2, ref1, ref2, color_difference)
+    diff1, diff2 = closest_difference(
+        stain1, stain2, ref1, ref2, color_difference_method
+    )
+
+    if color_difference(stain1, stain2, method="cie_76") < single_stain_threshold:
+        # The detected dominant stains are too close, they probably came from a region
+        # stained with only one stain. Therefore, no color the difference is computed
+        # for the second stain.
+        diff1 = min(diff1, diff2)
+        diff2 = 0
 
     result["stain_diff1"] = diff1
     result["stain_diff2"] = diff2
