66e21da538c5b620ca63342932ee6eabc178316a
Samuel Tichý <jovyan@vm-samuel-0.vm-samuel.rationai-ns.svc.cluster.local>
2024-12-01 19:22:35 +0100

    fix: typing, naming

--- rationai/qc/focus/focus_score_piqe.py
+++ rationai/qc/focus/focus_score_piqe.py
@@ -3,33 +3,34 @@ from piqe import piqe
 from skimage.color import rgb2gray
 from skimage.filters import median
 
-from rationai.qc.typing import QcValues, RGBImage
+from rationai.qc.typing import FocusScore, GrayScaleImage, RGBImage
 
 
-def _get_score_mask(gray, activity_mask):
+def _get_score_mask(
+    grayscale_img: GrayScaleImage, activity_mask: GrayScaleImage
+) -> GrayScaleImage:
     # TODO use standard tissue_mask library function instead
-    foreground = gray <= 0.96
-    foreground_pixels = np.sum(foreground)
+    foreground = grayscale_img <= 0.96
+    foreground_area = np.sum(foreground)
 
-    result = np.ones_like(gray)
+    score_mask = np.ones_like(grayscale_img)
     rebalanced_score = (
-        (np.sum(activity_mask * foreground) / foreground_pixels)
-        if foreground_pixels > 0
+        (np.sum(activity_mask * foreground) / foreground_area)
+        if foreground_area > 0
         else 0
     )
-    result.fill(rebalanced_score)
+    score_mask.fill(rebalanced_score)
 
-    return result
+    return score_mask
 
 
-def focus_score_piqe(img: RGBImage, pixel_size: float = 0.44) -> QcValues:
+def focus_score_piqe(img: RGBImage, pixel_size: float = 0.44) -> FocusScore:
     """Creates mask of the focus score based on the PIQE algorithm.
 
     Args:
         img: RGB Image of the tissue, minimum size 10x10 pixels.
         pixel_size: Size of a pixel in micrometers. The score is most accurate
-            at pixel sizes around 0.44 micrometers, which is equivalent to
-             a level 1 downsample of all slides this function has been tested on.
+            at pixel sizes around 0.44 micrometers.
 
     Returns:
         Dictionary with the focus score mask.
@@ -42,10 +43,14 @@ def focus_score_piqe(img: RGBImage, pixel_size: float = 0.44) -> QcValues:
         |-----------------------|---------------------------|
         | `focus_score_piqe`    | Mask of the focus score.  |
     """
-    result: QcValues = {}
+    result: FocusScore = {}
 
-    kernel_size = 2 * round(1 / pixel_size * 0.44 + 1e-9) + 1
-    gray = median(
+    # Used for calculating the kernel size
+    # for the median filter based on the pixel size
+    optimal_pixel_size = 0.44
+
+    kernel_size = 2 * round(1 / pixel_size * optimal_pixel_size + 1e-9) + 1
+    grayscale_img = median(
         rgb2gray(img), footprint=np.ones((kernel_size, kernel_size), dtype=bool)
     )
     (
@@ -54,14 +59,14 @@ def focus_score_piqe(img: RGBImage, pixel_size: float = 0.44) -> QcValues:
         _,  # Noise mask
         activity_mask,
     ) = piqe(
-        gray * 255
-    )  # gray is pixel array with values between 0 and 1, piqe expects values between 0 and 255
+        grayscale_img * 255
+    )  # grayscale_img is pixel array with values between 0 and 1, piqe expects values between 0 and 255
 
     # Activity mask is one column shorter for images
     # with dimensions that are not divisible by 16
-    if gray.shape != activity_mask.shape:
+    if grayscale_img.shape != activity_mask.shape:
         activity_mask = np.pad(activity_mask, ((0, 0), (0, 1)), mode="edge")
 
-    result["focus_score_piqe"] = _get_score_mask(gray, activity_mask)
+    result["focus_score_piqe"] = _get_score_mask(grayscale_img, activity_mask)
 
     return result
