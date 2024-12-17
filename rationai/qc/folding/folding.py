2b9016a88a9107eb38d30ba1e27bae78c5886a88
Erik Sedlák <521008@mail.muni.cz>
2024-12-17 14:47:13 +0100

    feat: Added new features

--- rationai/qc/folding/folding.py
+++ rationai/qc/folding/folding.py
@@ -3,49 +3,43 @@ from numpy.ma import MaskedArray
 from numpy.typing import NDArray
 from skimage.color import rgb2hsv
 from skimage.filters import threshold_yen
-from skimage.morphology import binary_opening, disk, reconstruction
+from skimage.measure import label
+from skimage.morphology import binary_closing, binary_opening, disk, reconstruction
 
 from rationai.qc.typing import QcValues, RGBImage
 from rationai.staining import ColorConversion, convert_color
 
 
-def _get_threshold(
-    img: NDArray[np.float32],
-    mask: NDArray[bool],  # type: ignore[PGH003]
-    local_tiles: NDArray[np.float32] | None = None,
-    local_mask: NDArray[bool] | None = None,  # type: ignore[PGH003]
-) -> float:
-    """Calculates adequate threshold from given images.
+def _get_mean_std_from_hist(n: NDArray[np.uint32], bins: NDArray[np.int32]):
+    """Calculates mean and standard deviation from histogram.
 
     Args:
-        img  : A given channel of an image.
-        mask : Background mask of an image.
-        local_tiles : Optional n*n tiles in local neighbourhood of tile. Defaults to None.
-        local_mask : Optional n*n background mask of local_tiles. Defaults to None.
+        n : Frequency of occurence in the corresponding bin.
+        bins : The bins of the histogram
 
     Returns:
-        Value which can be used to threshold the image.
+        The mean and the standard deviation of the values.
     """
-    if local_tiles is None:
-        return threshold_yen(MaskedArray(img, ~mask).compressed())
-    return threshold_yen(MaskedArray(local_tiles, ~local_mask).compressed())
+    mids = 0.5 * (bins[1:] + bins[:-1])
+    mean = np.average(mids, weights=n)
+    var = np.average((mids - mean) ** 2, weights=n)
+    std = np.sqrt(var)
+    return mean, std
 
 
 def folding(
     img: RGBImage,
-    level_downsample: float,
-    hematoxylin_eosin_stained: bool,
-    tissue_mask: NDArray[bool],  # type: ignore[PGH003]
-    local_tiles: NDArray[np.uint8] | None = None,
-    local_mask: NDArray[bool] | None = None,  # type: ignore[PGH003]
-    nucleus_diameter_at_base_level: int = 30,
-) -> QcValues:
+    mpp: float,
+    tissue_mask: BinaryMask,
+    local_tiles: RGBImage,
+    local_mask: BinaryMask,
+    cell_nucleus_size: float = 7,
+) -> FoldArtifacts:
     """Creates a binary mask of folding artifacts.
 
     Args:
         img: RGB image of the tissue.
-        level_downsample: Downsample at the level at which the image is provided.
-        hematoxylin_eosin_stained: True if image is stained using Hematoxylin and Eosin.
+        mpp: Number of microns per pixel of image.
         tissue_mask: A mask, where the tissue is labeled 1 and the background 0,
             should be as pixel-precise as possible.
         local_tiles: A local area surrounding the given tile.
@@ -67,86 +61,119 @@ def folding(
 
     Examples:
     ```python
-    from skimage.data import immunohistochemistry
+    import numpy as np
+    from PIL import Image
 
-    from rationai.qc.folding.folding import folding
+    import pyvips
+    from rationai.masks import tissue_mask
 
 
-    img = immunohistochemistry()
-    tissue_mask = function_for_tissue_mask(img)
+    img = np.asarray(Image.open("fold.png").conver("RGB"))  # Investigated image
+    local_area_image = img_area = np.asarray(
+        Image.open("fold_area.png").convert("RGB")
+    )  # Local area of investigated image
 
-    result = folding(img, 8, False, tissue_mask)
+    img_mask = tissue_mask(pyvips.Image.new_from_array(img), mpp=pixel_size).numpy() > 0
 
-    mask = result["folding"]  # Contains values 0 and 1
+    img_area_mask = (
+        tissue_mask(pyvips.Image.new_from_array(local_area_img), mpp=pixel_size).numpy()
+        > 0
+    )
+
+    artifacts = folding(
+        img=img,
+        mpp=1.76,
+        hematoxylin_eosin_stained=True,
+        tissue_mask=img_mask,
+        local_tiles=local_area_img,
+        local_mask=img_area_mask,
+    )
+
+    mask = artifacts["folding"]  # Contains values 0 and 1
     ```
 
     """
-    result: QcValues = {}
+    cell_nucleus_size_in_img = cell_nucleus_size / mpp
 
-    tile = img
-    hsv_tile = rgb2hsv(tile)
-    saturation_channel, value_channel = hsv_tile[:, :, 1], hsv_tile[:, :, 2]
-    local_saturation_channel, local_value_channel, local_eosin_channel = (
-        None,
-        None,
-        None,
-    )
-    if local_tiles is not None:
-        hsv_local = rgb2hsv(local_tiles)
-        local_saturation_channel, local_value_channel = (
-            hsv_local[:, :, 1],
-            hsv_local[:, :, 2],
-        )
-        local_value_channel = 1 - local_value_channel
-    if hematoxylin_eosin_stained:
-        _, eosin_channel, _ = convert_color(tile, ColorConversion.RGB2HER)
-        if local_tiles is not None:
-            _, local_eosin_channel, _ = convert_color(
-                local_tiles, ColorConversion.RGB2HER
-            )
-    else:
-        eosin_channel = np.ones_like(tissue_mask)
-        if local_tiles is not None:
-            local_eosin_channel = np.ones_like(local_tiles)
-
+    _, e_channel, _ = convert_color(img, ColorConversion.RGB2HER)
+    hsv = rgb2hsv(img)
+    saturation_channel, value_channel = hsv[:, :, 1], hsv[:, :, 2]
     inverted_value_channel = 1 - value_channel
 
-    value_threshold = _get_threshold(
-        inverted_value_channel, tissue_mask, local_value_channel, local_mask
+    hsv_local_tiles = rgb2hsv(local_tiles)
+    local_saturation_channel, local_value_channel = (
+        hsv_local_tiles[:, :, 1],
+        hsv_local_tiles[:, :, 2],
     )
-    saturation_threshold = _get_threshold(
-        saturation_channel, tissue_mask, local_saturation_channel, local_mask
+    _, local_eosin_channel, _ = convert_color(local_tiles, ColorConversion.RGB2HER)
+
+    bins = np.linspace(0, 1, 257)
+
+    local_saturation_histogram = np.histogram(
+        MaskedArray(local_saturation_channel, ~local_mask).compressed(), bins
     )
-    if hematoxylin_eosin_stained:
-        eosin_threshold = _get_threshold(
-            eosin_channel, tissue_mask, local_eosin_channel, local_mask
-        )
-    else:
-        eosin_threshold = 0
+    local_value_histogram = np.histogram(
+        MaskedArray(1 - local_value_channel, ~local_mask).compressed(), bins
+    )
+    local_eosin_histogram = np.histogram(
+        MaskedArray(local_eosin_channel, ~local_mask).compressed(), bins
+    )
+
+    saturation_threshold = threshold_yen(hist=local_saturation_histogram)
+    eosin_threshold = threshold_yen(hist=local_eosin_histogram)
+    value_threshold = threshold_yen(hist=local_value_histogram)
 
     thresholded_saturation = saturation_channel > saturation_threshold
+    thresholded_eosin = e_channel > eosin_threshold
     thresholded_value = inverted_value_channel > value_threshold
-    thresholded_eosin = eosin_channel > eosin_threshold
 
-    result["thresholded_saturation"] = thresholded_saturation
-    result["thresholded_value"] = thresholded_value
-    result["thresholded_eosin"] = thresholded_eosin
+    number_of_foreground_pixels = np.sum(tissue_mask)
 
-    if (
-        np.sum(thresholded_value) * 2 > tile.size
-        or np.sum(thresholded_saturation) * 2 > tile.size
-        or (hematoxylin_eosin_stained and np.sum(thresholded_eosin) * 2 > tile.size)
+    eosin_values, _ = np.histogram(
+        MaskedArray(e_channel, ~tissue_mask).compressed(), bins=bins
+    )
+
+    current_mean, _ = _get_mean_std_from_hist(eosin_values, bins)
+    local_mean_without_center = 1
+    local_std_without_center = 1
+    if np.all(local_eosin_histogram[0] >= eosin_values) and np.any(
+        local_eosin_histogram[0] > eosin_values
     ):
-        thresholded_value = np.zeros(tile.shape)
+        local_mean_without_center, local_std_without_center = _get_mean_std_from_hist(
+            local_eosin_histogram[0] - eosin_values, bins
+        )
+
+    if not (current_mean > local_mean_without_center + local_std_without_center):
+        if np.sum(thresholded_saturation) * 2 > number_of_foreground_pixels:
+            thresholded_saturation = np.zeros_like(tissue_mask)
+        if np.sum(thresholded_value) * 2 > number_of_foreground_pixels:
+            thresholded_saturation = np.zeros_like(tissue_mask)
+        if np.sum(thresholded_eosin) * 2 > number_of_foreground_pixels:
+            thresholded_eosin = np.zeros_like(tissue_mask)
 
     folding_test_markers = binary_opening(
         thresholded_eosin & thresholded_saturation & thresholded_value,
-        disk(nucleus_diameter_at_base_level // level_downsample),
+        disk(cell_nucleus_size_in_img),
     )
 
-    if hematoxylin_eosin_stained:
-        folding_test = reconstruction(folding_test_markers, thresholded_eosin)
-        result["folding"] = folding_test
-        return result
-    result["folding"] = folding_test_markers
+    reconstructed_markers = reconstruction(folding_test_markers, thresholded_eosin)
+
+    labeled_img, num_of_labels = label(reconstructed_markers, return_num=True)
+
+    for i in range(1, num_of_labels):
+        labeled_region = labeled_img == i
+        if np.sum(
+            binary_closing(labeled_region, disk(cell_nucleus_size_in_img * 5))
+        ) > 1.5 * np.sum(labeled_region):
+            labeled_img[labeled_img == i] = 0
+
+    folding_test = labeled_img > 0
+
+    result: FoldArtifacts = {
+        "folding": folding_test,
+        "thresholded_saturation": thresholded_saturation,
+        "thresholded_eosin": e_channel,
+        "thresholded_value": thresholded_value,
+    }
+
     return result
