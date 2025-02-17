8d1cb10c54d95c002808c469f0ae3938889585d6
Erik Sedlák <521008@mail.muni.cz>
2025-02-17 11:06:49 +0100

    feat: Decomposition

--- rationai/qc/__init__.py
+++ rationai/qc/__init__.py
@@ -1,5 +1,5 @@
 from rationai.qc.focus.focus_score_piqe import focus_score_piqe
-from rationai.qc.folding.folding import folding
+from rationai.qc.folding.folding import folding, folding_algorithm
 from rationai.qc.residual_artifacts import residual_artifacts_and_coverage
 from rationai.qc.staining import (
     dominant_stains,
@@ -12,6 +12,7 @@ __all__ = [
     "dominant_stains",
     "focus_score_piqe",
     "folding",
+    "folding_algorithm",
     "is_slide_correctly_stained",
     "residual_artifacts_and_coverage",
     "staining_difference",
---
+++ rationai/qc/folding/__init__.py
@@ -0,0 +1,7 @@
+from rationai.qc.folding.folding import folding, folding_algorithm
+
+
+__all__ = [
+    "folding",
+    "folding_algorithm",
+]
